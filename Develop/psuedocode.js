7e3bc4511f0e5c19d194df4d2f9a9a945d6f32f0
Rajveer Singh <rajsidhu2924@gmail.com>
2022-03-01 20:04:14 -0800

    intial commit

---
+++ Develop/psuedocode.js
@@ -0,0 +1,28 @@
+// user clicks button then prompt pops up asking for number of characters
+
+// hold all inputs from confirms in seperate variables
+
+// Do i need to hold any information?
+// create array with special characters
+// create array with numeric,
+// create array lowercase
+// create array upper case
+
+// create empty array that holds all the true confirmed options in it.
+// push all character that user wants into this array. This creates the pool of characters to select from
+// once the pool is complete, then randomly select characters based on the number of characters the user selcted in the beggining.
+
+// generate password function
+// var numberOfChar = validatecharLength(prompts for number of characters)
+// var isSpecialCharaters = confirm pops up asking if they would like specialial characters
+// var isCapitol = confirm for capital,
+// var isLowercase = lowercase confirm,
+// var isNumeric = numeric characters confirm
+
+// validate number of chars is between 8 and 128
+// validator function send it users input
+
+// validate user selected at least one of the availbale options
+// if user selected isSpecial or isCapital or isLower or isNum then continue
+// else alert user "none selected" and return to main function call
+

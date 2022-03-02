6c07852943ac001c07621798a2d59d39d8f201c6
Rajveer Singh <rajsidhu2924@gmail.com>
2022-03-01 20:07:05 -0800

    Arrays made in JS file

---
+++ Develop/script.js
@@ -0,0 +1,13 @@
+// selecting the button id from HTML 
+let generateBtn = document.querySelector("#generate");
+
+// array with lowercase alphabets
+const isLowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// array with uppercase alphabets
+const isUpperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+// array with numbers
+const isNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// an array with special characters.
+const isSpecialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
+// empty array that holds all the confirmed options in it.
+const charHolder = [];
